f90d35c46f027a4eaa979db727634651f7d72bd2
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-07-27 16:46:35 +0500

    added headers for cors policy

--- api/artist.ts
+++ api/artist.ts
@@ -9,6 +9,15 @@ export default async function handler(requst: VercelRequest, response: VercelRes
     const params = new URLSearchParams();
     params.append('grant_type', 'client_credentials');
 
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+
+    if (requst.method === 'OPTIONS') {
+        response.status(200).end();
+        return;
+    }
+
     const token = await axios.post<SpotifyToken>("https://accounts.spotify.com/api/token", params, {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
