4c18597ef91feeadabf1ea16fbd0400140253e23
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-07-29 21:43:24 +0500

    endpoint refactoring

---
+++ api/getArtistAlbums.ts
@@ -0,0 +1,22 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import axios from 'axios'
+import { handleCorsAndOptions } from '../lib/setCorsHeaders';
+import spotifyAuthSingleton from '../lib/spotifyAuth';
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+    const artistId = request.query.id as string;
+
+    if (handleCorsAndOptions(request, response)) return;
+
+    const token = await spotifyAuthSingleton.getToken()
+
+    const artistAlbums = await axios.get<SpotifyAlbumsResponse>(`https://api.spotify.com/v1/artists/${artistId}/albums`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }).then((response) => response.data);
+
+    return response.json({
+        artistAlbums
+    });
+}
